da258a322f0f6fc92c2aff579b6505e269318fcb
qddegtya <qddegtya@gmail.com>
2019-03-08 17:38:32 +0800

    fix: fix lifecycle name

--- src/framework/Page/mixins/lifecycle.js
+++ src/framework/Page/mixins/lifecycle.js
@@ -2,35 +2,35 @@ import NotImplementedError from '../__internal__/NotImplementedError'
 
 const lifecycle = {
   async onCreate(page) {
-    throw new NotImplementedError('onPageCreate')
+    throw new NotImplementedError('onCreate')
   },
 
   async onStart(page) {
-    throw new NotImplementedError('onPageStart')
+    throw new NotImplementedError('onStart')
   },
 
   async onPause(page) {
-    throw new NotImplementedError('onPagePause')
+    throw new NotImplementedError('onPause')
   },
 
   async onDestroy(page) {
-    throw new NotImplementedError('onPageDestroy')
+    throw new NotImplementedError('onDestroy')
   },
 
   async onRestart(page) {
-    throw new NotImplementedError('onPageRestart')
+    throw new NotImplementedError('onRestart')
   },
 
   async onResume(page) {
-    throw new NotImplementedError()
+    throw new NotImplementedError('onResume')
   },
 
   async onStop(page) {
-    throw new NotImplementedError()
+    throw new NotImplementedError('onStop')
   },
 
   async onRefresh(page, isRunning) {
-    throw new NotImplementedError()
+    throw new NotImplementedError('onRefresh')
   }
 }
 
