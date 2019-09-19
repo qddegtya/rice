9c1b42dcd82918dcb604e489073f0b477835522d
qddegtya <qddegtya@gmail.com>
2019-09-19 11:28:40 +0800

    chore: fix lint

--- packages/bootstrap/src/constants.js
+++ packages/bootstrap/src/constants.js
@@ -1,8 +1,8 @@
-const NOOP = () => undefined;
+const NOOP = () => undefined
 
 const DEFAULT_APP_OPTIONS = {
   mountNode: null,
   props: {}
-};
+}
 
-export { NOOP, DEFAULT_APP_OPTIONS };
+export { NOOP, DEFAULT_APP_OPTIONS }
--- packages/bootstrap/src/index.js
+++ packages/bootstrap/src/index.js
@@ -1,6 +1,6 @@
-import Rice from '@arice/core';
-import { internal } from 'xajs';
-import { NOOP, DEFAULT_APP_OPTIONS } from './constants';
+import Rice from '@arice/core'
+import { internal } from 'xajs'
+import { NOOP, DEFAULT_APP_OPTIONS } from './constants'
 
 const bootstrap = ({
   framework = {},
@@ -10,42 +10,40 @@ const bootstrap = ({
   beforeBootstrap = NOOP,
   afterBootstrap = NOOP
 }) => async appName => {
-  const rf = new Rice.Framework(framework);
+  const rf = new Rice.Framework(framework)
 
   for (const k in viewEngine) {
-    const ve = viewEngine[k];
-
-    if (!ve.isRiceView) throw new Error(`viewEngine: ${k} is not a valid rice view engine.`);
-    
-    rf.defineView(k, ve);
+    const ve = viewEngine[k]
+    if (!ve.isRiceView) throw new Error(`viewEngine: ${k} is not a valid rice view engine.`)
+    rf.defineView(k, ve)
   }
 
   if (appName in apps) {
     let { bundle: bundlePromise, options = DEFAULT_APP_OPTIONS } = apps[
       appName
-    ];
+    ]
 
-    let getBundle = () => null;
+    let getBundle = () => null
 
     if (internal.is.isFunction(bundlePromise)) {
-      getBundle = () => bundlePromise().then(mod => mod.default);
+      getBundle = () => bundlePromise().then(mod => mod.default)
     }
 
-    const app = await getBundle();
+    const app = await getBundle()
 
     if (!app) {
-      throw new Error(`Bundle of [${appName}] load failed, please check.`);
+      throw new Error(`Bundle of [${appName}] load failed, please check.`)
     }
 
-    await beforeBootstrap();
+    await beforeBootstrap()
 
-    const { mountNode = null, props = {} } = options;
-    await rf.loadApp(app, mountNode, props, plugins);
+    const { mountNode = null, props = {} } = options
+    await rf.loadApp(app, mountNode, props, plugins)
 
-    await afterBootstrap();
+    await afterBootstrap()
   } else {
-    throw new Error(`Can not find this app: ${appName}.`);
+    throw new Error(`Can not find this app: ${appName}.`)
   }
-};
+}
 
-export default bootstrap;
+export default bootstrap
