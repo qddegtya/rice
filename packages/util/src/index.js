0f98544e5f6e706808f1ba8de81781d11ee7ad98
qddegtya <qddegtya@gmail.com>
2019-09-27 19:14:35 +0800

    fix: fix renderSync

--- packages/util/src/index.js
+++ packages/util/src/index.js
@@ -1,9 +1,11 @@
 import NotImplementedError from './NotImplementedError'
 import message from './message'
 import * as xajs from 'xajs'
+import { render } from '@arice/platform-react'
 
 export {
   message,
   NotImplementedError,
-  xajs
+  xajs,
+  render as renderSync
 }
