ee37d2c55fc060e725a6e7dee1808dcc0fef080f
qddegtya <qddegtya@gmail.com>
2019-09-27 19:21:27 +0800

    refactor: remove platform-react from util

--- packages/util/src/index.js
+++ packages/util/src/index.js
@@ -1,11 +1,9 @@
 import NotImplementedError from './NotImplementedError'
 import message from './message'
 import * as xajs from 'xajs'
-import { render } from '@arice/platform-react'
 
 export {
   message,
   NotImplementedError,
-  xajs,
-  render as renderSync
+  xajs
 }
